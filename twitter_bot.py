618c2dcae2bf803a233696ba3c59d5aca7367ed8
SDR-mania2 <55984656+SDR-mania2@users.noreply.github.com>
2019-10-26 12:21:25 +0900

    Create twitter_bot.py

---
+++ twitter_bot.py
@@ -0,0 +1,21 @@
+import twitter
+
+
+#以下でTwitter APIのキーを指定
+auth = twitter.OAuth(consumer_key="",
+consumer_secret="",
+token="",
+token_secret="")
+
+t = twitter.Twitter(auth=auth)
+
+#以下で投稿するツイート指定
+bunsho="これはテストツイートです。"
+t.statuses.update(status=bunsho)
+
+#時間指定はcrontabで瞬殺
+#import os
+#os.system("echo '${* * * * * twitter_bot.pyのパス}' >> '${crontabファイルのパス}'")
+#みたいな感じで。
+#crond入れんとだめだが。
+
