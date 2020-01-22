737b1df58265495109983c465da61326dc080b17
SDR-mania2 <55984656+SDR-mania2@users.noreply.github.com>
2020-01-22 13:33:36 +0900

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,17 +1,18 @@
 import twitter
 
-#以下四カ所でTwitter APIのキーを指定
+# 以下四カ所でTwitter APIのキーを指定
 auth = twitter.OAuth(consumer_key="key1",consumer_secret="key2",token="key3",token_secret="key4")
 
 t = twitter.Twitter(auth=auth)
 
-#以下で投稿するツイート指定
+# 以下で投稿するツイート指定
 bunsho="これはテストツイートです。"
 t.statuses.update(status=bunsho)
 
-#時間指定はcrontabで瞬殺
-#import os
-#os.system("echo '0 * * * * * twitter_bot.pyのパス' >> crontabファイルのパス")
-#みたいな感じで。
-#crond入れんとだめだが。
-#Twython使えばもっと簡単なような気がする。
+# 時間指定はcrontabで瞬殺
+# import os
+# os.system("echo '0 * * * * * twitter_bot.pyのパス' >> crontabファイルのパス")
+# みたいな感じで。
+# crond入れんとだめだが。
+# Twython使えばもっと簡単なような気がする。
+# まだ動作検証してません。
